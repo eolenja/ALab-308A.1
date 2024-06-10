e7ef3cfc0207280070787e5baa89d7409ba701d7
eolenja <167790428+eolenja@users.noreply.github.com>
2024-06-10 16:42:10 -0400

    created try/catch function

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-let count=0
+let count=0;
+const incrementCount = (num) => {
+    
+try{
+    console.log (count)
+    count++
+    incrementCount()
+} catch (error) {
+    console.error (error)
+    console.log (count)
+}}
+incrementCount()
